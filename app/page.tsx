21e39045505a21e3859dcb5bc0bc9f1394448121
kotletkazla <akrapovitch69@gmail.com>
2025-11-28 16:16:25 +0100

    Create app/page.tsx with home page component for MMA fights

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>MMA Live Fights</h1>
+      <p>Welcome to the home page showcasing live MMA fights!</p>
+      {/* You can add more functionality here, such as an API call to get live fight data */}
+    </div>
+  );
+};
+
+export default HomePage;
